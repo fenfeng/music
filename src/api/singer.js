7e3529219706d4266927bbd738687a6489d17e49
fenfeng <1209364392@11.com>
2017-08-11 22:29:02 +0800

    6.0 commit

--- src/api/singer.js
+++ src/api/singer.js
@@ -12,7 +12,26 @@ export function getSingerList() {
     pagenum: 1,
     hostUin: 0,
     needNewCode: 0,
-    platform: 'yqq'
+    platform: 'yqq',
+    g_tk: 1664029744
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 80,
+    songstatus: 1,
+    singermid: singerId,
+    g_tk: 1664029744
   })
 
   return jsonp(url, data, options)
